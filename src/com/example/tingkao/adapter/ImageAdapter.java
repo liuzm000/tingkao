d7d98e36f7d6b666fec796d13145a1498e4c190d
liuzm000 <984460236@qq.com>
2016-01-08 10:20:09 +0800

    Revert "android开发中常见的效果"
    This reverts commit c397eaca56112019145c56da62bef6f6b4679208.

--- src/com/example/tingkao/adapter/ImageAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.example.tingkao.adapter;
-
-import android.widget.BaseAdapter;
-
-public class ImageAdapter{
-	
-}
--- src/com/example/tingkao/animation/CircleObjectAnimator.java
+++
@@ -1,7 +0,0 @@
-package com.example.tingkao.animation;
-
-import com.nineoldandroids.animation.ObjectAnimator;
-
-public class CircleObjectAnimator {
-
-}
